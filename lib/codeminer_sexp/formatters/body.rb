66d8389553b40e66e83d10c3c4193af608a0483c
Jacob Richardson <jacob.ninja.dev@gmail.com>
2014-07-11 10:54:35 -0600

    Condition formatter

---
+++ lib/codeminer_sexp/formatters/body.rb
@@ -0,0 +1,13 @@
+module CodeMiner
+  module Formatters
+
+    class Body < SexpFormatter
+
+      def type
+        :block
+      end
+
+    end
+
+  end
+end
---
+++ lib/codeminer_sexp/formatters/condition.rb
@@ -0,0 +1,35 @@
+module CodeMiner
+  module Formatters
+
+    class Condition < SexpFormatter
+
+      def type
+        :if
+      end
+
+      def each
+        [exp.test, consequence, else_expression]
+      end
+
+      def consequence
+        exp.consequence and wrap_body(exp.consequence)
+      end
+
+      def else_expression
+        exp.else_statement and wrap_body(exp.else_statement)
+      end
+
+      private
+
+      def wrap_body(exp)
+        if exp.each.count == 1
+          exp.each.first
+        else
+          Body.new(exp, @parser)
+        end
+      end
+
+    end
+
+  end
+end
